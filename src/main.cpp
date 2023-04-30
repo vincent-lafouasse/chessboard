584c2e67d302f88e66b05dda532b868d157b3c75
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-30 20:53:20 +0200

    printable board

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,82 @@
+#include <stdint.h>
+#include <array>
 #include <iostream>
 
-enum Piece {
-  Pawn = 0,
-  Bishop = 1,
-  Knight = 2,
-  Rook = 3,
-  Queen = 4,
-  King = 5,
+#define N_SQUARES 64
+
+typedef uint8_t Piece;
+typedef std::array<Piece, 64> Board;
+
+enum {
+  None = 0,
+  Pawn = 1,
+  Bishop = 2,
+  Knight = 3,
+  Rook = 4,
+  Queen = 5,
+  King = 6,
   White = 8,
   Black = 16,
 };
 
+char piece_repr(Piece piece) {
+  char output;
+  switch (piece % 8) {
+    case None: {
+      output = '.';
+      break;
+    }
+    case Pawn: {
+      output = 'P';
+      break;
+    }
+    case Bishop: {
+      output = 'B';
+      break;
+    }
+    case Knight: {
+      output = 'N';
+      break;
+    }
+    case Rook: {
+      output = 'R';
+      break;
+    }
+    case Queen: {
+      output = 'Q';
+      break;
+    }
+    case King: {
+      output = 'K';
+      break;
+    }
+  }
+
+  if (piece % 8 != None && (piece | White) != White) {
+    output += 32;
+  }
+  return output;
+}
+
+Board new_board(void) {
+  Board board;
+  for (int i = 0; i < N_SQUARES; i++) {
+    board[i] = None;
+  }
+  return board;
+}
+
+void print_board(Board board) {
+  for (int i = 0; i < N_SQUARES; i++) {
+    std::cout << piece_repr(board[i]);
+    if ((i + 1) % 8 == 0) {
+      std::cout << std::endl;
+    }
+  }
+  std::cout << std::endl;
+}
+
 int main(void) {
-  std::cout << (Bishop | White) << std::endl;
+  Board board = new_board();
+  print_board(board);
 }
