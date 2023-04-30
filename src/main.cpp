4d61486811be5a64306671c2e4d950abe349e17e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-04-30 20:35:00 +0200

    Piece enum

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+enum Piece {
+  Pawn = 0,
+  Bishop = 1,
+  Knight = 2,
+  Rook = 3,
+  Queen = 4,
+  King = 5,
+  White = 8,
+  Black = 16,
+};
+
 int main(void) {
-  std::cout << "bjr" << std::endl;
+  std::cout << (Bishop | White) << std::endl;
 }
